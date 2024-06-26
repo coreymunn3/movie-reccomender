ac7aa3991df32fc1cb5084eb5cb6d0538112fcfc
Corey Munn <michael.munn@secunetics.com>
2024-06-26 12:30:08 -0400

    created HomePage with list of movies in db. had to updated next config for image source paths

---
+++ src/components/MoviePoster/MoviePoster.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import { Box, Text } from "@chakra-ui/react";
+
+const MoviePoster = (props) => {
+  const { Title, Genre, Poster, Year, imdbID } = props;
+  return (
+    <Box h="100%" p={"0.5rem"}>
+      <Image
+        src={Poster}
+        alt={Title}
+        width={300}
+        height={300}
+        style={{ width: "100%", borderRadius: "4px" }}
+      />
+      <Text color="white" as="b" fontSize={"sm"}>{`${Title} (${Year})`}</Text>
+      <Text color="gray.200" fontSize={"sm"}>
+        {Genre}
+      </Text>
+    </Box>
+  );
+};
+
+export default MoviePoster;
