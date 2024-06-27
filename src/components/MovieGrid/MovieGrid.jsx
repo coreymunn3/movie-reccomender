4e6de6e0a876340d59791f1d83b9da4a4365a1f4
Corey Munn <michael.munn@secunetics.com>
2024-06-27 09:15:56 -0400

    implement ability to show a Ranking in MovieGrid

--- src/components/MovieGrid/MovieGrid.jsx
+++ src/components/MovieGrid/MovieGrid.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { SimpleGrid, Box } from "@chakra-ui/react";
 import MoviePoster from "@/components/MoviePoster/MoviePoster";
+import RankDisplay from "../RankDisplay/RankDisplay";
 
 const MovieGrid = (props) => {
-  const { movies } = props;
+  const { movies, showRank } = props;
   return (
     <SimpleGrid columns={{ lg: 4, md: 3, sm: 2 }} gap={6}>
-      {movies.map((movie) => (
+      {movies.map((movie, index) => (
         <Box
           w="100%"
           h="100%"
@@ -14,7 +15,9 @@ const MovieGrid = (props) => {
           display={"flex"}
           justifyContent={"center"}
           alignItems={"flex-start"}
+          position={"relative"}
         >
+          {showRank && <RankDisplay rank={index + 1} />}
           <MoviePoster {...movie} />
         </Box>
       ))}
