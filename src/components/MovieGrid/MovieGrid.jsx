f8beb165ca89bee6b2eae82a7b9d68803147bc06
Corey Munn <michael.munn@secunetics.com>
2024-06-27 12:32:17 -0400

    move Rank into moviePoster & include similarity score in moviePoster

--- src/components/MovieGrid/MovieGrid.jsx
+++ src/components/MovieGrid/MovieGrid.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { SimpleGrid, Box } from "@chakra-ui/react";
 import MoviePoster from "@/components/MoviePoster/MoviePoster";
-import RankDisplay from "../RankDisplay/RankDisplay";
 
 const MovieGrid = (props) => {
   const { movies, showRank } = props;
   return (
     <SimpleGrid columns={{ lg: 4, md: 3, sm: 2 }} gap={6}>
-      {movies.map((movie, index) => (
-        <Box
-          w="100%"
-          h="100%"
-          key={movie._id}
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"flex-start"}
-          position={"relative"}
-        >
-          {showRank && <RankDisplay rank={index + 1} />}
-          <MoviePoster {...movie} />
-        </Box>
-      ))}
+      {movies.map((movie, index) => {
+        const rank = showRank ? index + 1 : undefined;
+        return (
+          <Box
+            w="100%"
+            h="100%"
+            key={movie._id}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"flex-start"}
+          >
+            <MoviePoster {...movie} rank={rank} />
+          </Box>
+        );
+      })}
     </SimpleGrid>
   );
 };
--- src/components/MoviePoster/MoviePoster.jsx
+++ src/components/MoviePoster/MoviePoster.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Box, Text } from "@chakra-ui/react";
+import RankDisplay from "../RankDisplay/RankDisplay";
+import SimilarityDisplay from "../SimilarityDisplay/SimilarityDisplay";
 
 const MoviePoster = (props) => {
-  const { Title, Genre, Poster, Year, imdbID, ...movie } = props;
+  const { rank, Title, Genre, Poster, Year, imdbID, $similarity, ...movie } =
+    props;
   return (
     <Link href={`/movie/${movie._id}`}>
       <Box h="100%">
@@ -12,6 +15,7 @@ const MoviePoster = (props) => {
           _hover={{
             boxShadow: "10px 10px 20px rgba(36, 36, 36, 0.5)",
           }}
+          position={"relative"}
         >
           <Image
             src={Poster}
@@ -20,6 +24,8 @@ const MoviePoster = (props) => {
             height={300}
             style={{ width: "100%", borderRadius: "4px" }}
           />
+          {rank && <RankDisplay rank={rank} />}
+          {$similarity && <SimilarityDisplay similarity={$similarity} />}
         </Box>
 
         <Text color="white" as="b" fontSize={"sm"}>{`${Title} (${Year})`}</Text>
