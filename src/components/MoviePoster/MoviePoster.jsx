1cebbcbc4c8604197f280b78d24260e6f1d6c7e0
Corey Munn <michael.munn@secunetics.com>
2024-06-27 10:08:26 -0400

    convert moviePoster to link to movie page

--- src/components/MoviePoster/MoviePoster.jsx
+++ src/components/MoviePoster/MoviePoster.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { Box, Text } from "@chakra-ui/react";
 
 const MoviePoster = (props) => {
-  const { Title, Genre, Poster, Year, imdbID, ...movieProps } = props;
+  const { Title, Genre, Poster, Year, imdbID, ...movie } = props;
   return (
-    <Box h="100%">
-      <Image
-        src={Poster}
-        alt={Title}
-        width={300}
-        height={300}
-        style={{ width: "100%", borderRadius: "4px" }}
-      />
-      <Text color="white" as="b" fontSize={"sm"}>{`${Title} (${Year})`}</Text>
-      <Text color="gray.200" fontSize={"sm"}>
-        {Genre}
-      </Text>
-    </Box>
+    <Link href={`/movie/${movie._id}`}>
+      <Box h="100%">
+        <Image
+          src={Poster}
+          alt={Title}
+          width={300}
+          height={300}
+          style={{ width: "100%", borderRadius: "4px" }}
+        />
+        <Text color="white" as="b" fontSize={"sm"}>{`${Title} (${Year})`}</Text>
+        <Text color="gray.200" fontSize={"sm"}>
+          {Genre}
+        </Text>
+      </Box>
+    </Link>
   );
 };
 
