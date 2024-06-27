e7d867c7e68742c2aa20a1df7a9a3c14b5b1a21c
Corey Munn <michael.munn@secunetics.com>
2024-06-27 09:16:36 -0400

    create movie search page with list of movies and their ranking in search results

--- src/pages/search/[term]/index.js
+++ src/pages/search/[term]/index.js
@@ -1,5 +1,41 @@
 import React from "react";
+import db from "@/db";
+import { Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import MovieGrid from "@/components/MovieGrid/MovieGrid";
 
 export default function Search(props) {
-  return <p>Hello, search</p>;
+  const { movies } = props;
+  console.log(movies);
+  const router = useRouter();
+
+  return (
+    <React.Fragment>
+      <Text align={"center"} color="white" marginY={"2rem"}>
+        Suggest results based on your search
+      </Text>
+      <MovieGrid movies={movies} showRank={true} />
+    </React.Fragment>
+  );
 }
+
+export const getServerSideProps = async (context) => {
+  const moviesColl = db.collection("movies");
+  const similarMovies = await moviesColl
+    .find(
+      {},
+      {
+        sort: {
+          $vectorize: context.query.term,
+        },
+        limit: 10,
+      }
+    )
+    .toArray();
+
+  return {
+    props: {
+      movies: similarMovies,
+    },
+  };
+};
